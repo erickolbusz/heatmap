d7cc114fde7bd8b1e8cc63f34b73f4f666225f20
erickolbusz <erickolbusz@gmail.com>
2020-07-02 22:33:39 -0400

    coordinate system and value reading

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-var heatmap = h337.create({
-  container: document.querySelector('#fullscreen-div'),
-  backgroundColor: "black",
-  radius: 5
-});
-
 MINX = -5;
 MAXX = 5;
 MINY = -3;
 MAXY = 3;
 
-DX = 5;
-DY = 5;
+DX = 10;
+DY = 10;
 
-var pixelToCoord = function(px,py) {
-
-}
 var genHeat = function(f) {
-  //generate heatmap for f(x,y) given coords on screen
+  //generate points for heatmap of f(x,y)
   var points = [];
   var max = -1*Infinity;
   var min = Infinity;
   var val, x, y;
   for (var px=0; px<window.innerWidth; px+=DX) {
     for (var py=0; py<window.innerHeight; py+=DY) {
+      pyInv = window.innerHeight - py;
       x = (px/window.innerWidth)*MAXX + (1-(px/window.innerWidth))*MINX;
-      y = (py/window.innerHeight)*MAXY + (1-(py/window.innerHeight))*MINY;
-      val = f.evaluate( {x:x, y:y} );
+      y = (pyInv/window.innerHeight)*MAXY + (1-(pyInv/window.innerHeight))*MINY;
+      val = math.re(f.evaluate( {x:x, y:y} ));
       if (val > max) { max = val; }
       if (val < min) { min = val; }
       points.push({
@@ -35,7 +27,6 @@ var genHeat = function(f) {
       });
     }
   }
-  console.log(min, max);
   return {max: max, min: min, data: points};
 }
 
@@ -44,8 +35,37 @@ var plotFunction = function() {
   heatmap.setData(genHeat(equation));
 }
 
+var updateCoordBox = function(event) {
+  var coordBox = document.querySelector('#coordBox');
+  //get mouse coords
+  var mx = event.clientX;
+  var my = event.clientY;
+  //round to the nearest computed point and find value
+  var px = DX*Math.round(mx/DX);
+  var py = DY*Math.round(my/DY);
+  var pyInv = window.innerHeight - py;
+  var val = Math.round(1000*heatmap.getValueAt({ x:px, y:py }))/1000; //theyre int only :(
+  //change coordBox text and position
+  x = Math.round(1000*((px/window.innerWidth)*MAXX + (1-(px/window.innerWidth))*MINX))/1000;
+  y = Math.round(1000*((pyInv/window.innerHeight)*MAXY + (1-(pyInv/window.innerHeight))*MINY))/1000;
+  coordBox.innerHTML = "f("+x+", "+y+") = "+val;
+}
+
+var clearCoordBox = function(event) {
+  coordBox.innerHTML = "";
+}
+
+//callbacks
 document.querySelector('#funcButton').onclick = plotFunction;
+document.querySelector('#mouse-div').onmousemove = updateCoordBox;
+document.querySelector('#mouse-div').onmouseout = clearCoordBox;
 
 //init
+var heatmap = h337.create({
+  container: document.querySelector('#heatmap-div'),
+  backgroundColor: "black",
+  radius: 12
+});
+
 document.querySelector('#funcBox').value = "x+500*cos(y)";
 plotFunction();
